b7fb2d996eb1770fc56c02a9b29159c1db680645
andrepazvi <andrea.paz.villarroel@gmail.com>
2023-08-21 15:03:55 -0400

    primera entrega

--- index.js
+++ index.js
@@ -1,39 +1,82 @@
-const objetos = [
-    {
-      manzanas: 3,
-      peras: 2,
-      carne: 1,
-      jugos: 5,
-      dulces: 2,
-    },
-    {
-      manzanas: 1,
-      sandias: 1,
-      huevos: 6,
-      jugos: 1,
-      panes: 4,
-    },
-  ];
+class ProductManager {
+    constructor() {
+      this.products = [];
+      this.nextId = 1;
+    }
   
-  // Creo un nuevo array con todos los tipos de productos
-  const TipoDeProductos = objetos.reduce((types, obj) => {
-    Object.keys(obj).forEach((productType) => {
-      if (!types.includes(productType)) {
-        types.push(productType);
+    addProduct(product) {
+      if (!this.validateProduct(product)) {
+        console.log("Error: Producto no valido.");
+        return;
       }
-    });
-    return types;
-  }, []);
   
-  // Muestro el nuevo array por consola
-  console.log("Tipos de productos:", TipoDeProductos);
+      if (this.isCodeDuplicate(product.code)) {
+        console.log("Error: el código del producto ya existe.");
+        return;
+      }
   
-  // Obtengo el total de productos vendidos
-  const totalProductosVendidos = objetos.reduce((total, obj) => {
-    const cantidades = Object.values(obj);
-    const sumCantidades = cantidades.reduce((sum, cantidad) => sum + cantidad, 0);
-    return total + sumCantidades;
-  }, 0);
+      product.id = this.nextId++;
+      this.products.push(product);
+      console.log("Producto añadido:", product);
+    }
   
-  console.log("Total de productos vendidos:", totalProductosVendidos);
+    validateProduct(product) {
+      return (
+        product.title &&
+        product.description &&
+        product.price &&
+        product.thumbnail &&
+        product.code &&
+        product.stock !== undefined
+      );
+    }
+  
+    isCodeDuplicate(code) {
+      return this.products.some((product) => product.code === code);
+    }
+  
+    getProducts() {
+      return this.products;
+    }
+  
+    getProductById(id) {
+      const product = this.products.find((product) => product.id === id);
+      if (product) {
+        return product;
+      } else {
+        console.log("Error: Producto no encontrado.");
+      }
+    }
+  }
+  
+  const productManager = new ProductManager();
+  
+  const product1 = {
+    title: "Producto 1",
+    description: "Descripción del producto 1",
+    price: 10,
+    thumbnail: "image1.jpg",
+    code: "P1",
+    stock: 50,
+  };
+  
+  const product2 = {
+    title: "Producto 2",
+    description: "Descripción del producto 2",
+    price: 20,
+    thumbnail: "image2.jpg",
+    code: "P2",
+    stock: 30,
+  };
+  
+  productManager.addProduct(product1);
+  productManager.addProduct(product2);
+  
+  const allProducts = productManager.getProducts();
+  console.log("All products:", allProducts);
+  
+  const productById = productManager.getProductById(1);
+  console.log("Product by ID:", productById);
+  
+  const productNotFound = productManager.getProductById(123);
   
